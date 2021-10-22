fe75e910a57ca4b11c65ed18f59aad9000b22866
Casey Halstead <catherineclosee@gmail.com>
2021-10-22 12:01:57 -0500

    Edit key syntax

--- models/modelA.js
+++ models/modelA.js
@@ -1,10 +1,10 @@
 var airport = {
-  fullName = "Kennedy International Airport",
-  abbreviation = "JFK",
-  totalTerminals = 9,
-  totalPassengers = 625e5,
-  isOpen = true,
-  isBusy = true,
-  carriers = ["American Airlines", "Delta Air Lines", "JetBlue"],
-  directFlights = ["Los Angeles, CA", "Miami, FL", "Dallas, TX", "Chicago, IL"]
+  fullName: "Kennedy International Airport",
+  abbreviation: "JFK",
+  totalTerminals: 9,
+  totalPassengers: 625e5,
+  isOpen: true,
+  isBusy: true,
+  carriers: ["American Airlines", "Delta Air Lines", "JetBlue"],
+  directFlights: ["Los Angeles, CA", "Miami, FL", "Dallas, TX", "Chicago, IL"]
 };
