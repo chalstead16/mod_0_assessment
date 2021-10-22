00899a51afb56b1b8c8547058f7fc84d5df726cd
Casey Halstead <catherineclosee@gmail.com>
2021-10-22 12:04:15 -0500

    Add vehicle object model

---
+++ models/modelB.js
@@ -0,0 +1,10 @@
+var vehicle = {
+  make: "Jeep",
+  model: "Cherokee",
+  seats: 5,
+  totalPassengers: 5,
+  isEmpty: true,
+  isHeavy: true,
+  colorOptions: ["white", "black", "red"],
+  seatCoverMaterialOptions: ["leather", "nylon", "vinyl"]
+};
