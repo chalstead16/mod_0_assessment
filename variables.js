6799873a94269be5c0706eb9227832bfb59a1bae
Casey Halstead <catherineclosee@gmail.com>
2021-10-22 11:56:24 -0500

    Complete program specific challenge

---
+++ variables.js
@@ -0,0 +1,16 @@
+var famousPerson = "Will Smith";
+
+var isActive = true;
+
+famousPerson = "Paul Rudd";
+
+var num3 = 3;
+
+var num9 =9;
+
+var sum = num3 + num9;
+
+var animals = ["chicken", "pig", "cow"];
+animals.shift();
+
+console.log(`${famousPerson.length}`);
